a774a3135c88f6ddde30f22aa28959ecc369e105
Yamik <wbf.robit@gmail.com>
2020-06-05 18:44:45 +0500

    почистил файлы

---
+++ src/components/component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="get-input">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'get-input',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
